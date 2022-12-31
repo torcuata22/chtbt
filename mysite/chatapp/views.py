5299ac540ddd02fbf7f280585738f3381af4b8ce
Marilyn Marquez <marilynmarquez@MMarquez.local>
2022-12-31 13:34:39 -0600

    fourth

--- mysite/chatapp/views.py
+++ mysite/chatapp/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .models import ChatRoom
 
 # Create your views here.
 def index(request):
-    return render(request, 'chatapp/index.html')
+    chatrooms = ChatRoom.objects.all() 
+    return render(request, 'chatapp/index.html', {'chatrooms': chatrooms})
+
+def chatroom(request, slug):
+    chatroom = ChatRoom.objects.get(slug=slug)
+    return render(request, 'chatapp/room.html', {'chatroom':chatroom})
