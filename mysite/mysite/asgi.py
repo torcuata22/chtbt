646deb1883dac0be3f4e0aecb6219b6cd2f029b3
Marilyn Marquez <marilynmarquez@MMarquez.local>
2022-12-31 16:14:38 -0600

    web sockets are connecting to chat rooms

--- mysite/mysite/asgi.py
+++ mysite/mysite/asgi.py
@@ -10,7 +10,28 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack #auth middleware import
+from channels.routing import ProtocolTypeRouter, URLRouter #second import needed
+import chatapp.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
-application = get_asgi_application()
+#With websockets:
+application = ProtocolTypeRouter({
+    'http': get_asgi_application(),
+    'websocket':AuthMiddlewareStack(
+        URLRouter(
+            chatapp.routing.websocket_urlpatterns
+        )
+    ),
+    
+}) #setting HTTP protocol and websocket, so app could use both
+
+
+# BEFORE:application = get_asgi_application()
+
+
+#modifications to work with web sockets:
+#imports
+#connect routing.py
+#websocket_urlpatters are the ones defined in routing.py
