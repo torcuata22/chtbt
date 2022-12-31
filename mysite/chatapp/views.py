a40838325e055bba23c45580ebeb1cd1e9edd94b
Marilyn Marquez <marilynmarquez@MMarquez.local>
2022-12-31 13:58:09 -0600

    fifth

--- mysite/chatapp/views.py
+++ mysite/chatapp/views.py
@@ -6,6 +6,8 @@ def index(request):
     chatrooms = ChatRoom.objects.all() 
     return render(request, 'chatapp/index.html', {'chatrooms': chatrooms})
 
-def chatroom(request, slug):
+def chatroom(request,slug):
     chatroom = ChatRoom.objects.get(slug=slug)
-    return render(request, 'chatapp/room.html', {'chatroom':chatroom})
+    #chatroom = ChatRoom.objects.get(slug=slug)
+    #return render(request, 'chatapp/room.html', {'chatroom':chatroom})
+    return render(request,'chatapp/room.html',{'chatroom':chatroom})
