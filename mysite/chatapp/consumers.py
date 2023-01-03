eff226c75b8670ccdf94236a7589540ff1b02668
Marilyn Marquez <marilynmarquez@MMarquez.local>
2023-01-03 10:30:11 -0600

    setting up consimer to receive and sen messages

--- mysite/chatapp/consumers.py
+++ mysite/chatapp/consumers.py
@@ -21,8 +21,41 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
     #function to handle disconnecting:
     async def disconnect(self):
-        #remove group fomr channel:
+        #remove group from channel:
         await self.channel_layer.group_discard(
             self.channel_layer,
             self.room_group_name
         )
+
+    #handles what happens when we receive a particular message:
+    async def receive(self, message_data):
+        data = json.loads(message_data) #loads the data as json
+        #decode the data it received:
+        message = data['message']
+        username = data['username']
+        room = data['room']
+
+        #create function to send data to the channel_layer group:
+        await self.channel_layer.group_send(
+            self.room_group_name, {
+                'type':'chat_message',
+                'message':message,
+                'username': username, 
+                'room': room
+            }
+        )
+
+    #send message to the room:
+    async def chat_message(self, event):
+        message = event['message']
+        username = event['username']
+        room = event['room']
+
+        await self.send(text_data = json.dumps({
+            'message': message,
+            'username': username,
+            'room': room,
+        }))
+
+#consumer has been configured to receive a message and send it back to the client
+#we need to receive the message inside the client and display it
